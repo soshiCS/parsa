19443f9e2e8e2446a6c2a29d332b6ab330c506f7
Soroush Aghajani <43025972+soshiCS@users.noreply.github.com>
2024-07-15 18:34:51 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Fetch a random Quran verse
-    const quranApiUrl = 'https://api.alquran.cloud/v1/ayah/random';
-    fetch(quranApiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const verse = data.data.text;
-            const surah = data.data.surah.englishName;
-            const ayah = data.data.numberInSurah;
-            document.getElementById('quote').innerText = `“${verse}” - Surah ${surah}, Ayah ${ayah}`;
-        })
-        .catch(error => console.error('Error fetching the Quran verse:', error));
-
     // Function to get the current date as an integer (e.g., 20240715)
     function getCurrentDate() {
         const date = new Date();
